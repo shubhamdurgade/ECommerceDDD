16b8f0a86e4a58685c4a1c10e0906c48ea98b4c3
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-03-10 23:32:59 +0530

    feat(domain): add repository interfaces and order domain service
    - Added ICustomerRepository, IOrderRepository, and IProductRepository interfaces
    - Implemented OrderDomainService in domain services
    - Updated ECommerce.Infrastructure project configuration

---
+++ ECommerce.Domain/Repositories/IOrderRespository.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface IOrderRespository
+    {
+        Task<Order?> GetByIdAsync(int id);
+
+        Task AddAsync(Order order);
+    }
+}
---
+++ ECommerce.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce_Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(int id);
+
+        Task<IEnumerable<Product>> GetAllAsync();
+
+        Task AddAsync(Product product);
+
+        Task UpdateAsync(Product product);
+    }
+}
