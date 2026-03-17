5da9f034ed35b0788b2571b1c15d1ffbc511132d
Shubham Durgade <shubhamdurgade1203@gmail.com>
2026-03-17 23:53:17 +0530

    Implement the Order and OrderItem DTO and Product Reposistory.

---
+++ ECommerce Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Domain.Repositories;
+using ECommerce_Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore; 
+
+namespace ECommerce_Infrastructure.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ECommerceDbContext _dbContext;
+        public ProductRepository(ECommerceDbContext dbContext) 
+        { 
+            _dbContext = dbContext;
+        }
+        public async Task<Product> AddAsync(Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _dbContext.Products.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Products.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _dbContext.Products.Update(product);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
